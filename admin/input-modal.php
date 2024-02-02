307427562689c60842c1378f6a358f7ef08f5035
Arnadi <49811141+masadiblog@users.noreply.github.com>
2024-02-02 13:12:43 +0700

    Add files via upload

--- admin/input-modal.php
+++ admin/input-modal.php
@@ -1,18 +1,24 @@
 <div class="container">
-  
-  <form autocomplete="off" id="input-data" class="input-data border border-info shadow rounded p-3">
-    <h5 class="border-bottom border-info pb-2 text-center">Input Produk</h5>
-    <div class="row">
-      <div class="col-6">
-        <label for="nama" class="form-label">Merek</label>
-        <input type="search" id="nama" class="form-control" placeholder="Merek...">
+  <h1 class="fs-6 mb-0">Input Data Produk</h1>
+  <form autocomplete="off" id="input-data" class="bg-info input-data border border-info shadow rounded px-1 pt-0 pb-1">
+    <div class="row mb-2">
+      <div class="col-12">
+        <label for="nama" class="form-label mb-0">Nama / Merek</label>
+        <input type="search" id="nama" class="form-control">
         <div class="err"></div>
       </div>
-      <div class="col-6 ps-0">
-        <label for="modal" class="form-label">Modal</label>
+    </div>
+    <div class="row">
+      <div class="col-5">
+        <label for="modal" class="form-label mb-0">Harga Modal</label>
+        <input type="search" id="modal" class="form-control">
+        <div class="err"></div>
+      </div>
+      <div class="col-7 ps-1">
+        <label for="modal" class="form-label mb-0 d-flex justify-content-between">Harga Jual <span id="ssha" class="badge bg-light text-primary fw-normal" style="height:19px"></span></label>
         <div class="d-flex">
-          <input type="search" id="modal" class="form-control me-2" placeholder="Modal...">
-          <button type="submit" class="btn text-bg-info" style="width:42px"><i class="fas fa-angle-right"></i></button>
+          <input type="search" id="jual" class="form-control me-3">
+          <button type="submit" class="btn text-bg-secondary" style="width:50px"><i class="fas fa-angle-right"></i></button>
         </div>
         <div class="err"></div>
       </div>
@@ -26,7 +32,7 @@
   ?>
   <div class="mt-2 mb-3">
     <div class="input-group shadow">
-      <input type="search" placeholder="Cari Nama/Merek" id="cari-data" class="form-control">
+      <input type="search" placeholder="Masukkan Nama/Merek" id="cari-data" class="form-control">
       <span class="input-group-text">
         <a href="<?=$base_url;?>" class="text-decoration-none" style="font-size:.8em">
           <div class="d-inline-block" style="width:10px"><i class="fas fa-square-plus"></i></div> Penjualan
@@ -41,12 +47,13 @@
           <th class="text-bg-info text-center">No</th>
           <th class="text-bg-info">Merek</th>
           <th class="text-bg-info">Modal</th>
+          <th class="text-bg-info">Jual</th>
           <th colspan="2" class="text-bg-info text-center" style="width:60px"><i class="fas fa-chevron-down"></i></th>
         </tr>
       </thead>
       <tbody id="resmo">
         <tr>
-          <td colspan="5" class="text-center py-1"><?=mysqli_num_rows($sel_modal);?> data tersimpan</td>
+          <td colspan="6" class="text-center py-1"><?=mysqli_num_rows($sel_modal);?> data tersimpan</td>
         </tr>
 <?php
 $no = 1;
@@ -54,13 +61,19 @@ while($row_modal = mysqli_fetch_array($sel_modal)){
   $id_modal = $row_modal['id_modal'];
   $nama = $row_modal['nama'];
   $modal = number_format($row_modal['modal'],0,',','.');
+  if($row_modal['jual'] != ''){
+    $jual = number_format($row_modal['jual'],0,',','.');
+  }else{
+    $jual = '';
+  }
 ?>
         <tr>
           <td valign="middle" class="text-center"><?=$no;?></td>
           <td valign="middle"><?=$nama;?></td>
           <td valign="middle"><?=$modal;?></td>
-          <td style="width:32px" valign="middle" id="editData" class="text-center" data-bs-toggle="modal" data-bs-target="#editModal" data-id="<?=$id_modal;?>" data-nama="<?=$nama;?>" data-modal="<?=$modal;?>"><i class="fas fa-edit text-success"></i></td>
-          <td style="width:32px" valign="middle" id="hapusData" class="text-center" data-id="<?=$id_modal;?>" data-baris="<?=$no;?>" data-nama="<?=$nama;?>" data-modal="<?=$modal;?>"><i class="fas fa-trash text-danger"></i></td>
+          <td valign="middle"><?=$jual;?></td>
+          <td style="width:32px" valign="middle" id="editData" class="text-center" data-bs-toggle="modal" data-bs-target="#editModal" data-id="<?=$id_modal;?>" data-nama="<?=$nama;?>" data-modal="<?=$modal;?>" data-jual="<?=$jual;?>"><i class="fas fa-edit text-success"></i></td>
+          <td style="width:32px" valign="middle" id="hapusData" class="text-center" data-id="<?=$id_modal;?>" data-baris="<?=$no;?>" data-nama="<?=$nama;?>" data-modal="<?=$modal;?>" data-jual="<?=$jual;?>"><i class="fas fa-trash text-danger"></i></td>
         </tr>
 <?php
   $no++;
@@ -78,13 +91,20 @@ while($row_modal = mysqli_fetch_array($sel_modal)){
             <h5 class="border-bottom pb-2">Edit Data</h5>
             <div class="mb-3">
               <label for="nama" class="form-label">Nama / Merek</label>
-              <input type="search" id="nama" class="form-control" placeholder="Masukkan Nama / Merek...">
+              <input type="search" id="nama" class="form-control">
               <div class="err"></div>
             </div>
-            <div class="mb-3">
-              <label for="modal" class="form-label">Harga Modal</label>
-              <input type="search" id="modal" class="form-control" placeholder="Masukkan Harga Modal...">
-              <div class="err"></div>
+            <div class="row mb-3">
+              <div class="col-6 pe-1">
+                <label for="modal" class="form-label">Harga Modal</label>
+                <input type="search" id="modal" class="form-control">
+                <div class="err"></div>
+              </div>
+              <div class="col-6 ps-0">
+                <label for="jual" class="form-label d-flex justify-content-between">Harga Jual <span id="ssha2" class="badge bg-light text-primary fw-normal" style="height:19px"></span></label>
+                <input type="search" id="jual" class="form-control">
+                <div class="err"></div>
+              </div>
             </div>
             <div class="text-end">
               <button type="button" id="batalEdit" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
