d3f5f6ad6de4850ff47421ff77d752efa075e954
Arnadi <49811141+masadiblog@users.noreply.github.com>
2024-02-02 19:31:18 +0700

    Add files via upload

--- admin/input-modal.php
+++ admin/input-modal.php
@@ -1,11 +1,15 @@
-<div class="container">
+<div class="container px-1 pb-1">
   <h1 class="fs-6 mb-0">Input Data Produk</h1>
   <form autocomplete="off" id="input-data" class="bg-info input-data border border-info shadow rounded px-1 pt-0 pb-1">
     <div class="row mb-2">
-      <div class="col-12">
+      <div class="col-9">
         <label for="nama" class="form-label mb-0">Nama / Merek</label>
         <input type="search" id="nama" class="form-control">
         <div class="err"></div>
+      </div>
+      <div class="col-3 ps-1">
+        <label for="stok" class="form-label mb-0">Stok</label>
+        <input type="search" id="stok" class="form-control px-2">
       </div>
     </div>
     <div class="row">
@@ -41,19 +45,20 @@
     </div>
   </div>
   <div class="tab-scroll mb-4 border border-info shadow" style="max-height:52vh">
-    <table class="table table-striped mb-0">
-      <thead>
-        <tr style="position:sticky;top:0">
-          <th class="text-bg-info text-center">No</th>
+    <table class="table table-striped mb-0" style="font-size:.85em">
+      <thead class="bg-info">
+        <tr class="border-info">
+          <th class="text-bg-info text-center psl">No</th>
           <th class="text-bg-info">Merek</th>
           <th class="text-bg-info">Modal</th>
           <th class="text-bg-info">Jual</th>
-          <th colspan="2" class="text-bg-info text-center" style="width:60px"><i class="fas fa-chevron-down"></i></th>
+          <th class="text-bg-info">Stok</th>
+          <th class="text-bg-info text-center psr"><i class="fas fa-chevron-down"></i></th>
         </tr>
       </thead>
       <tbody id="resmo">
         <tr>
-          <td colspan="6" class="text-center py-1"><?=mysqli_num_rows($sel_modal);?> data tersimpan</td>
+          <td colspan="6" class="text-bg-secondary text-center py-1"><?=mysqli_num_rows($sel_modal);?> data tersimpan</td>
         </tr>
 <?php
 $no = 1;
@@ -66,14 +71,21 @@ while($row_modal = mysqli_fetch_array($sel_modal)){
   }else{
     $jual = '';
   }
+  if($row_modal['stok'] != ''){
+    $stok = number_format($row_modal['stok'],0,',','.');
+  }else{
+    $stok = '0';
+  }
 ?>
         <tr>
-          <td valign="middle" class="text-center"><?=$no;?></td>
+          <td valign="middle" class="text-center psl"><?=$no;?></td>
           <td valign="middle"><?=$nama;?></td>
           <td valign="middle"><?=$modal;?></td>
           <td valign="middle"><?=$jual;?></td>
-          <td style="width:32px" valign="middle" id="editData" class="text-center" data-bs-toggle="modal" data-bs-target="#editModal" data-id="<?=$id_modal;?>" data-nama="<?=$nama;?>" data-modal="<?=$modal;?>" data-jual="<?=$jual;?>"><i class="fas fa-edit text-success"></i></td>
-          <td style="width:32px" valign="middle" id="hapusData" class="text-center" data-id="<?=$id_modal;?>" data-baris="<?=$no;?>" data-nama="<?=$nama;?>" data-modal="<?=$modal;?>" data-jual="<?=$jual;?>"><i class="fas fa-trash text-danger"></i></td>
+          <td valign="middle"><?=$stok;?></td>
+          <td valign="middle" class="text-center psr">
+            &nbsp;<span id="editData" data-bs-toggle="modal" data-bs-target="#editModal" data-id="<?=$id_modal;?>" data-nama="<?=$nama;?>" data-modal="<?=$modal;?>" data-jual="<?=$jual;?>" data-stok="<?=$stok;?>"><i class="fas fa-edit text-success"></i></span>&nbsp;<span id="hapusData" data-id="<?=$id_modal;?>" data-baris="<?=$no;?>" data-nama="<?=$nama;?>" data-modal="<?=$modal;?>" data-jual="<?=$jual;?>"><i class="fas fa-trash text-danger"></i></span>
+          </td>
         </tr>
 <?php
   $no++;
@@ -89,10 +101,16 @@ while($row_modal = mysqli_fetch_array($sel_modal)){
         <div class="modal-body">
           <form autocomplete="off" id="edit-data">
             <h5 class="border-bottom pb-2">Edit Data</h5>
-            <div class="mb-3">
-              <label for="nama" class="form-label">Nama / Merek</label>
-              <input type="search" id="nama" class="form-control">
-              <div class="err"></div>
+            <div class="row mb-3">
+              <div class="col-9 pe-1">
+                <label for="nama" class="form-label">Nama / Merek</label>
+                <input type="search" id="nama" class="form-control">
+                <div class="err"></div>
+              </div>
+              <div class="col-3 ps-0">
+                <label for="stok" class="form-label">Stok</label>
+                <input type="search" id="stok" class="form-control px-2">
+              </div>
             </div>
             <div class="row mb-3">
               <div class="col-6 pe-1">
